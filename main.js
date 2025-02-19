3aa97c65a58f22f3ba4a9791658acc0b94e7f725
Dhyuthi V <dew@Dhyuthis-Laptop.local>
2025-02-19 20:58:06 +0530

    made categories

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const { app, BrowserWindow } = require('electron');
 
 app.whenReady().then(() => {
   const win = new BrowserWindow({
-    width: 1200,
-    height: 800,
+    width: 560,
+    height: 710,
     webPreferences: {
       nodeIntegration: true
     }
